c9761bb9ed34b8a0a67fc6d5559c5c2ccb95b3e1
enekolizarraga <lizarragaansoeneko@gmail.com>
2025-04-14 19:16:58 +0200

    Create config.php

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+$dbhost = "localhost"; // Database Hostname
+$dbname = ""; // Database Name
+$user = ""; // Database Username
+$password = ""; // Database User's Password
+
+try {
+    $dsn = "mysql:host=$dbhost;dbname=$dbname";
+    $db = new PDO($dsn, $user, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+} catch (PDOException $e){
+    echo $e->getMessage();
+}
